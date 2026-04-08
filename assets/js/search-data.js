1e91524a5a9c8179942360702739feb35b6ce5f5
aadya29 <148147577+aadya29@users.noreply.github.com>
2026-04-08 16:29:53 +0000

    Deploying to gh-pages from @ aadya29/aadya29.github.io@62e43221a1af8838ac78c26d43036b72932539eb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Click the pdf icon on the right to download my full updated CV. Last updated on Apr 8, 2026.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
